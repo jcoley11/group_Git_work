0deac422f0a3d6813982f01b6a98b631be4af635
Jackson Coley <u1307995@granite1.chpc.utah.edu>
2025-09-17 14:22:17 -0600

    mleaningful message

--- folder1/hello.py
+++ folder1/hello.py
@@ -3,11 +3,14 @@ print("hello world!")
 print('making some serious changes here')
 
 
+
+
 print('Cam Newton')
 print('trying again.')
 
 
 print('this is a solution to the bug')
+
 
 
 
@@ -40,4 +43,9 @@ print('now trying with merge as opposed to rbase')
 
 
 print('changes to the new branch')
+
+
+print('changes to the new branch')
+
+
 
